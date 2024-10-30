f272e4b4a5956600545980a123608e5436901992
Your Name <tiendat1516@gmail.com>
2024-10-30 12:29:32 +0700

    ver 2.0

---
+++ CSD_Asignment2/src/model/Laptop.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Phan Sơn
+ */
+public class Laptop extends NetworkDevice {
+
+    private String password;
+
+    public Laptop(String name, String macAddress, int numOfPorts) {
+        super(name, macAddress, numOfPorts);
+    }
+// ------------------------------------------------------------------
+/*
+    Quang worksite inside here. Responsible for Class Laptop
+    */
+
+// ------------------------------------------------------------------
+
+}
